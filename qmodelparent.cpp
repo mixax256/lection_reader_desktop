7b32267e6039845489531633b873e7796b36084d
tagar <vmagdesyan@yandex.ru>
2016-10-05 15:19:54 +0300

    Создание стандартных конструкторов в модели

--- qmodelparent.cpp
+++ qmodelparent.cpp
@@ -1,6 +1,51 @@
 #include "qmodelparent.h"
+#include "qdatabasework.h"
 
-QModelParent::QModelParent(QObject *parent) : QObject(parent)
+
+QModelParent::QModelParent(string dbName)
 {
-    
+    QDataBaseWork db;
+    db.createDataBase(dbName);
+
 }
+
+QModelParent::~QModelParent()
+{
+
+}
+
+QVariant QModelParent::data(const QModelIndex &index, int role) const
+{
+
+}
+int QModelParent::rowCount(const QModelIndex &parent) const
+{
+    if (!parent.isValid()){
+        return 0;
+    }
+    const DataWrapper* parentInfo = static_cast<const DataWrapper*>(parent.internalPointer());
+    return parentInfo->children.size();
+}
+
+int QModelParent::columnCount(const QModelIndex &parent) const
+{
+    return 0;
+}
+
+QModelIndex QModelParent::index(int row, int column, const QModelIndex &parent) const
+{
+    if (!hasIndex(row, column, parent)){
+        return QModelIndex();
+    }
+    if (!parent.isValid()) {
+        return createIndex(row, column, const_cast<DataWrapper*>(&d.children[row]));
+    }
+    DataWrapper* parentInfo = static_cast<DataWrapper*>(parent.internalPointer());
+    return createIndex(row, column, &parentInfo->children[row]);
+}
+
+QModelIndex QModelParent::parent(const QModelIndex &child) const
+{
+
+}
+
--- qmodelparent.h
+++ qmodelparent.h
@@ -2,16 +2,33 @@
 #define QMODELPARENT_H
 
 #include <QObject>
-
-class QModelParent : public QObject
+#include <QAbstractItemModel>
+#include <QModelIndex>
+using namespace std;
+class QModelParent : public QAbstractItemModel
 {
+    enum h_type {ROOT, COURSE, THEME, IMAGE};
     Q_OBJECT
 public:
-    explicit QModelParent(QObject *parent = 0);
+    QModelParent(string dbName);
+    ~QModelParent();
+    struct DataWrapper{
+        quint16 id;
+        h_type type;
+        void *data;
+        int number;
+        DataWrapper *parent;
+        QList<DataWrapper> children;
+        int count;
+    };
 
-signals:
-
-public slots:
+    virtual QVariant data(const QModelIndex &index, int role) const;
+    virtual int rowCount(const QModelIndex &parent) const;
+    virtual int columnCount(const QModelIndex &parent) const;
+    virtual QModelIndex index(int row, int column, const QModelIndex &parent) const;
+    virtual QModelIndex parent(const QModelIndex &child) const;
+private:
+    DataWrapper d{0, ROOT, nullptr, 0, nullptr, {}, -1};
 };
 
 #endif // QMODELPARENT_H
